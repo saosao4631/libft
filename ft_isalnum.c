ac3bea8e3e8f23afb2507460ade02fce22193ed0
saosao4631 <saopan834@gmail.com>
2026-04-20 01:48:26 +0900

    aaa

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: ksaotome <ksaotome@student.42.ja>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 01:21:08 by ksaotome          #+#    #+#             */
-/*   Updated: 2026/04/20 01:21:58 by ksaotome         ###   ########.fr       */
+/*   Updated: 2026/04/20 01:47:27 by ksaotome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@ int	ft_isalnum(int c)
 	return (flag);
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int	main(void)
-{
-	char c1 = 'a';
-	char c2 = 'Z';
-	char c3 = '1';
-	char c4 = '@';
+// int	main(void)
+// {
+// 	char c1 = 'a';
+// 	char c2 = 'Z';
+// 	char c3 = '1';
+// 	char c4 = '@';
 
-	printf("%d\n", ft_isalnum(c1)); // 1
-	printf("%d\n", ft_isalnum(c2)); // 1
-	printf("%d\n", ft_isalnum(c3)); // 1
-	printf("%d\n", ft_isalnum(c4)); // 0
+// 	printf("%d\n", ft_isalnum(c1)); // 1
+// 	printf("%d\n", ft_isalnum(c2)); // 1
+// 	printf("%d\n", ft_isalnum(c3)); // 1
+// 	printf("%d\n", ft_isalnum(c4)); // 0
 
-	return (0);
-}
+// 	return (0);
+// }
