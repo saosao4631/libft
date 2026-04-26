31daa95961a2c5b0c6eb5642381e2528b2341907
Kazuki Saotome <ksaotome@c5r2s2.42tokyo.jp>
2026-04-26 15:16:54 +0900

    strmapi.cのセグフォ修正　s=null確認の前にlenを測定していた

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksaotome <ksaotome@student.42.ja>          +#+  +:+       +#+        */
+/*   By: ksaotome <ksaotome@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 13:12:53 by ksaotome          #+#    #+#             */
-/*   Updated: 2026/04/22 13:51:44 by ksaotome         ###   ########.fr       */
+/*   Updated: 2026/04/26 15:15:47 by ksaotome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	size_t			len;
 
-	len = ft_strlen(s);
 	if (!s || !f)
 		return (NULL);
+	len = ft_strlen(s);
 	res = (char *)malloc(len + 1);
 	if (!res)
 		return (NULL);
