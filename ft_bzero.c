6fe7dc556bfed5dd0dcabb8bd86e57b38138f6da
saosao4631 <saopan834@gmail.com>
2026-04-29 16:11:58 +0900

    aaa

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksaotome <ksaotome@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: ksaotome <ksaotome@student.42.ja>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 21:30:58 by ksaotome          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2026/04/29 13:42:56 by ksaotome         ###   ########.fr       */
-=======
-/*   Updated: 2026/04/25 16:15:29 by ksaotome         ###   ########.fr       */
->>>>>>> 64fb435ac456359dec7383bf2df2f8175aadb2a1
+/*   Updated: 2026/04/29 16:10:35 by ksaotome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +24,17 @@ void	ft_bzero(void *s, size_t n)
 	}
 }
 
-// void	ft_bzero(void *s, size_t n)
-// {
-// 	ft_memset(s, 0, n);
-// }
-
 // int main(void)
 // {
-// 	char str[20] = "Hello, World!";
-
-// 	char buf1[20] = "Hello, World!";
-// 	char buf2[20] = "Hello, World!";
+// 	char	buf1[20] = "Hello, World!";
+// 	char	buf2[20] = "Hello, World!";
 // 	ft_bzero(buf1, 20);
 // 	bzero(buf2, 20);
 // 	printf("ft_bzeroの場合: %s\n", buf1);
 // 	printf("bzeroの場合: %s\n", buf2);
 
-// 	printf("---\n指定数が0の場合\n\n");
-
-// 	char buf3[20] = "Hello, World!";
-// 	char buf4[20] = "Hello, World!";
+// 	char	buf3[20] = "Hello, World!";
+// 	char	buf4[20] = "Hello, World!";
 // 	ft_bzero(buf3, (0));
 // 	bzero(buf4, (0));
 // 	printf("ft_bzeroの場合: %s\n", buf3);
